afcc24fd45a083e97ae1d7a478b8c9900df0e62a
vaporvee <80621863+cmod31@users.noreply.github.com>
2022-02-23 10:18:40 +0100

    Files via Uploads because Visual Studio doesn't work

---
+++ Virus/Virus/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Virus
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            do
+            {
+                var result = MessageBox.Show("Ich lösch dich du Hurensohn 00000000000000000000000000000000000000000000000000000", "!!!!!!!VIRUS!!!!!!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    MessageBox.Show("Danke", "!!!!!!!VIRUS!!!!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                if (result == DialogResult.No)
+                {
+                    MessageBox.Show("Hurensohn", "!!!!!!!VIRUS!!!!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            } while (true);
+        }
+    }
+}
